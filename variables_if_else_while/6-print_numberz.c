aa6177ef86c256f5cb1f458a21ec85bfbb6c3dca
Souf <soufianmghirat@hotmail.com>
2024-02-23 14:43:46 +0100

    for putchar

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -8,25 +8,10 @@
 */
 int main(void)
 {
-putchar('0');
-putchar('\n');
-putchar('1');
-putchar('\n');
-putchar('2');
-putchar('\n');
-putchar('3');
-putchar('\n');
-putchar('4');
-putchar('\n');
-putchar('5');
-putchar('\n');
-putchar('6');
-putchar('\n');
-putchar('7');
-putchar('\n');
-putchar('8');
-putchar('\n');
-putchar('9');
+	for (n = 0; n++)
+{
+	putchar('0', '\n', 'n+1')
+}
 putchar('\n');
 return (0);
 }

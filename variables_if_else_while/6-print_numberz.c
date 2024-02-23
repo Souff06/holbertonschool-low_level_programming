67f1068f38e9445fc739404d7d39481ba72d6f14
Souf <soufianmghirat@hotmail.com>
2024-02-23 14:38:35 +0100

    putchar

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+* main - print if the number is postive, zero, or negative
+*
+* Description: using the main function
+* this program prints "Programming is positive, zero, or negative
+* Return: 0
+*/
+int main(void)
+{
+putchar('0');
+putchar('\n');
+putchar('1');
+putchar('\n');
+putchar('2');
+putchar('\n');
+putchar('3');
+putchar('\n');
+putchar('4');
+putchar('\n');
+putchar('5');
+putchar('\n');
+putchar('6');
+putchar('\n');
+putchar('7');
+putchar('\n');
+putchar('8');
+putchar('\n');
+putchar('9');
+putchar('\n');
+return (0);
+}

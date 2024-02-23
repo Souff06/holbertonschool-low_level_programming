12c5a9bd73f169a21e611752b6a5d61f274c5368
Souf <soufianmghirat@hotmail.com>
2024-02-23 12:47:29 +0100

    if else

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -6,15 +6,14 @@
 * this program prints "Programming is positive, zero, or negative
 * Return: 0
 */
-int main()
+int main(void)
 {
 	char ch;
-
-	for (ch = 'a'; ch <= 'z'; ch++) {
+	{
+	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
-	}
 
 	putchar('\n');
-
+	}
 	return 0;
 }

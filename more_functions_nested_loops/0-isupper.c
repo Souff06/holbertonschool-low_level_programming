f30bb2c1c6dec2414a450ac9e151c942e09d2a89
Souf <soufianmghirat@hotmail.com>
2024-02-26 13:30:27 +0100

    condition

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -2,18 +2,13 @@
 #include <stdio.h>
 #include <ctype.h>
 
-int _isupper(int c);
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * _isupper - check the code.
+ * @c: sdbckj
+ * Return: 1 quand c majusc sinon 0.
  */
 
 int _isupper(int c)
 {
-    /*
-    * Check if the character is an uppercase letter
-    */
-    return (c >= 'A' && c <= 'Z') ? 1 : 0;
+	return ((c >= 'A' && c <= 'Z') ? 1 : 0);
 }
-

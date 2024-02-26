c4665efdae212d17d6ab55d4e0377662c1dd6e4b
Souf <soufianmghirat@hotmail.com>
2024-02-26 14:50:18 +0100

    mul

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _isdigit - check the code.
+ * mul - check the code.
  * @a: nombre entiers
  * @b: nombre entiers
  * Return: multiplies les deux nombres.
  */
 int mul(int a, int b)
 {
-    return (a * b); mul(656, 24);
+return (a * b); mul(656, 24)
 }

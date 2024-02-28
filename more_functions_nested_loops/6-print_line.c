1895b19c3310a10573169a613db0688283866d2c
Souf <soufianmghirat@hotmail.com>
2024-02-28 14:24:37 +0100

    line _

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - faire une ligne avec des traits bas _
+ * @n: combien de fois se repete le _
+*/
+void print_line(int n)
+{
+	int c;
+
+	for (c = 0; c <= n; c++)
+	{
+		if (n > 0)
+			_putchar('_');
+	}
+	_putchar('\n');
+}

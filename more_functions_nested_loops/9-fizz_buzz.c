3735e3f14e629fc46bb7b689c3dffcecfc36271b
Souf <soufianmghirat@hotmail.com>
2024-02-28 16:53:04 +0100

    revoir

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - eazy en vrai
+ * Return: Always 0.
+*/
+
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % 3 == 0)
+			printf("Fizz ");
+		if (a % 5 == 0)
+			printf("Buzz ");
+		else if (a % 3 == 0 && a % 5 == 0)
+			printf("FizzBuzz ");
+		else
+			printf("%d ", a);
+	}
+	printf("\n");
+	return (0);
+}

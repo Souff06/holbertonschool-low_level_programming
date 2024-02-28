7d3f18ef232d9a19c0a2403e93135b7cb2a7686f
Souf <soufianmghirat@hotmail.com>
2024-02-28 13:41:53 +0100

    for while

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -8,15 +8,15 @@ void more_numbers(void)
 	int c;
 	int i;
 
-	while (i < 10)
+	for (i = 0; i < 10; i++)
 	{
 		for (c = 0; c <= 14; c++)
 		{
 			if (c >= 10)
 				_putchar((c / 10) + '0');
 			_putchar((c % 10) + '0');
+
 		}
 		_putchar('\n');
-		i++;
 	}
 }

58c441fe6f25ba8c9903164de86c81f44bd27a86
Souf <soufianmghirat@hotmail.com>
2024-03-01 12:33:34 +0100

    affichier a lenvers

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_rev - imprimer a l'envers
+ * @s: la chaine de caracteres
+*/
+
+void print_rev(char *s)
+{
+	int i = 0;
+	int j;
+
+	for (; s[i] != '\0';)
+		i++;
+	for (j = i--; j >= 0; j--)
+		_putchar(s[j]);
+	_putchar('\n');
+}

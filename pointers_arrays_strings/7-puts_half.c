dad2e3f114d9e02051d0e554bfb97b1fe1096d28
Souf <soufianmghirat@hotmail.com>
2024-03-01 14:35:51 +0100

    afficher la 2eme moitier de la cc

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * _strlen - affiche le nombre de caractere
+ * @s: compte le nombre dans i
+ * Return: le nombre de caracteres
+*/
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+/**
+ * puts_half - affiche la 2eme moitie de la chain carac
+ * @str: la chaine de carac
+ */
+
+void puts_half(char *str)
+{
+	int n = 0;
+	int longCh = _strlen(str);
+
+	if (longCh % 2 != 0)
+	{
+		n = (longCh - 1) / 2;
+		_putchar(n);
+	}
+	for (longCh = longCh / 2; str[longCh] != '\0'; longCh++)
+		_putchar(str[longCh]);
+	_putchar('\n');
+}

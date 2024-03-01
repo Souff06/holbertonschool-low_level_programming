9599d0b87980f26baad108b09837587ee7eae0cf
Souf <soufianmghirat@hotmail.com>
2024-03-01 13:58:18 +0100

    affiche en passant 2 par 2

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -6,9 +6,13 @@
 
 void puts2(char *str)
 {
-	int i;
+	int i = 0;
+	int j;
 
-	for (i = 0; str[i] != '\0'; i += 2)
-		_putchar(str[i]);
+	while (str[i] != '\0')
+		i++;
+	for (j = 0; j < i ; j++)
+		if (j % 2 == 0)
+			_putchar(str[j]);
 	_putchar('\n');
 }

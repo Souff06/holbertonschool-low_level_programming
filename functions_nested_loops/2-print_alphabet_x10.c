6e42577d527421fde61f23ba14049184d4104824
Souf <soufianmghirat@hotmail.com>
2024-03-04 11:22:43 +0100

    gg

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+*   print_alphabet_x10 - Prints the alphabet lowercase 10 times followed
+*                    by a new line
+*  Return: Always 0
+*/
+
+void print_alphabet_x10(void)
+{
+	int a;
+	char c = 'a';
+
+	for (a = 0; a < 10; a++)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+	}
+}

a4d36157be14a7d7cb43e3bd7ebb371411266b5b
Souf <soufianmghirat@hotmail.com>
2024-03-04 11:18:24 +0100

    gg

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - function to print abc
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	int a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		_putchar(a);
+	_putchar('\n');
+}

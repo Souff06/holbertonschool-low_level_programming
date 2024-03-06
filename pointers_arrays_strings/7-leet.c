21c2fb477246b2aa9860b91d1cf8aec0e4b86c4e
Souf <soufianmghirat@hotmail.com>
2024-03-06 16:13:12 +0100

    cava

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * leet - j'ai mis du temps zebi
+ * @s: string
+ * Return: on renvoie la chaine de carac(str)
+ */
+
+char *leet(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		while (s[i] == 'a' || s[i] == 'A')
+			s[i] = '4';
+		while (s[i] == 'e' || s[i] == 'E')
+			s[i] = '3';
+		while (s[i] == 'o' || s[i] == 'O')
+			s[i] = '0';
+		while (s[i] == 't' || s[i] == 'T')
+			s[i] = '7';
+		while (s[i] == 'l' || s[i] == 'L')
+			s[i] = '1';
+	}
+	return (s);
+}

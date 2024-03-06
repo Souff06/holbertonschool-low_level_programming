2133b6bc328e564110b31cc9ce90f8cc6be100f8
Souf <soufianmghirat@hotmail.com>
2024-03-06 17:03:14 +0100

    gg

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _memset - remplir un bloc de memoire
+ * @s: pointeur
+ * @b: l'octet constant
+ * @n: les n premiers octets de la zone mémoire pointée par s
+ * Return: return 0
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	memset(s, b, n);
+	return (0);
+}

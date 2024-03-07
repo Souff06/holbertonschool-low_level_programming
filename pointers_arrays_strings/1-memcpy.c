292b2022421cf44a9bd3eade287504d059f20095
Souf <soufianmghirat@hotmail.com>
2024-03-07 15:41:19 +0100

    gg

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -10,12 +10,8 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
-	unsigned int j;
 
 	for (i = 0; i < n; i++)
-	{
-		for (j = 0; j < n; j++)
-			src[i + j] = dest[j];
-	}
+		src[i] = dest[i];
 	return (dest);
 }

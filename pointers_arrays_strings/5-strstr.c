db39ef1dc573205efe752229953875a5157d9ce4
Souf <soufianmghirat@hotmail.com>
2024-03-07 19:49:48 +0100

    gg

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -12,11 +12,15 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 
-	for (i = 0; haystack[i] != 0; i++)
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0; needle[j] != 0; j++)
-		if (*needle == haystack[i + j])
-			return (needle);
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j])
+				break;
+		}
+		if (!needle[j])
+			return (&haystack[i]);
 	}
 	return (NULL);
 }

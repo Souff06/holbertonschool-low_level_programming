02f608be176cdde9ab2b421f758e889ded0635ce
Souf <soufianmghirat@hotmail.com>
2024-03-07 15:26:55 +0100

    gg

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -9,14 +9,11 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i;
+	unsigned int i;
 
-	i = 0;
-	while (n > 0)
+	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
-		s++;
-		n--;
 	}
 	return (s);
 }

c0bf42cca4520116ecaab576e52413516e67e22c
Souf <soufianmghirat@hotmail.com>
2024-03-08 16:16:02 +0100

    gg

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -9,13 +9,13 @@ int main(int argc, char *argv[])
 {
 	int i, mul = 1;
 
-	if (argc >= 1)
+	if (argc > 1)
 	{
 		for (i = 1; i < argc; i++)
 			mul = mul * atoi(argv[i]);
 		printf("%d\n", mul);
 	}
 	else
-		printf("Error");
+		printf("Error\n");
 	return (0);
 }

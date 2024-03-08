475719a50c233bed6b04fd2fe60f99347c78bd63
Souf <soufianmghirat@hotmail.com>
2024-03-08 15:55:42 +0100

    gg

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - argc/argv
+ * @argc: le count
+ * @argv: le nom de l'argu
+ * Return: result
+*/
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}

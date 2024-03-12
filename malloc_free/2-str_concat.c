7b7ecd89eb8d56d70f1c93b89964f17a0d32fc6f
Souf <soufianmghirat@hotmail.com>
2024-03-12 11:29:18 +0100

    gg

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -11,11 +11,10 @@ char *str_concat(char *s1, char *s2)
 	int i, j, c, d;
 	char *a;
 
-	if ((s1 == NULL) || (s2 == NULL))
-	{
+	if (s1 == NULL)
 		s1 = "";
+	if (s2 == NULL)
 		s2 = "";
-	}
 	for (i = 0; s1[i] != 0; i++)
 		;
 	for (j = 0; s2[j] != 0; j++)

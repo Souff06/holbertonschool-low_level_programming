f3572ec524cd9ee33ecffe8cd84d90e91c731077
Souf <soufianmghirat@hotmail.com>
2024-03-15 22:42:27 +0100

    gg

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+/**
+ * print_dog - initialise la structure dog.
+ * @d: nom de la structure
+*/
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		d->name ? (printf("Name: %s\n", d->name)) : (printf("Name: (nil)\n"));
+		printf("Age: %f\n", d->age);
+		d->owner ? (printf("Owner: %s\n", d->owner)) : (printf("Owner: (nil)\n"));
+	}
+}

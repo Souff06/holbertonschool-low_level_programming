fad021f616f874ed26a6e74a1b6b3fc5b57d441f
Souf <soufianmghirat@hotmail.com>
2024-03-20 16:26:47 +0100

    gg

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,33 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * get_op_func - contain the function that selects the correct function
+ * @s: pointeur
+ *
+ * Return: NULL
+*/
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	while (i <= 4)
+	{
+		if (*ops[i].op == *s)
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}

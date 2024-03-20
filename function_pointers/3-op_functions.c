f0446e40fd954fe316cf8524839d7c503f6eaa48
Souf <soufianmghirat@hotmail.com>
2024-03-20 16:26:37 +0100

    gg

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * op_add - returns the sum
+ * @a: number 1
+ * @b: number 2
+ * Return: result of the calcul
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - returns the sum
+ * @a: number 1
+ * @b: number 2
+ * Return: result of the calcul
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - returns the sum
+ * @a: number 1
+ * @b: number 2
+ * Return: result of the calcul
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - returns the sum
+ * @a: number 1
+ * @b: number 2
+ * Return: result of the calcul
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - returns the sum
+ * @a: number 1
+ * @b: number 2
+ * Return: result of the calcul
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}

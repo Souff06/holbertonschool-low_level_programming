6ed6640eb0b9dfc5013860a1999a584ce8f926af
Souf <soufianmghirat@hotmail.com>
2024-03-22 12:30:27 +0100

    gg

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,18 @@
+#include "variadic_functions.h"
+
+
+int sum_them_all(const unsigned int n, ...)
+{
+    int sum = 0;
+    unsigned int i;
+    va_list ptr;
+
+    if (n == 0)
+        return (0);
+    va_start (ptr, n);
+
+    for (i = 0; i < n; i++)
+        sum += va_arg(ptr, int);
+    va_end(ptr);
+    return (sum);
+}

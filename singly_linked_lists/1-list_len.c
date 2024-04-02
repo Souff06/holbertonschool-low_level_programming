05f77b816448908fffabb4d3de9dc7063b0e94fe
Souf <soufianmghirat@hotmail.com>
2024-04-02 11:07:08 +0200

    the number of element in the list

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * list_len - Counts the number of elements in a linked list.
+ * @h: pointer to the head of the linked list
+ * Return: the number of elements in the list
+*/
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
